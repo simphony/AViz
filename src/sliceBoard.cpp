a2c0f6e16513b4da21de8a89c1ccb99d1548bad6
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-01-09 18:05:42 +0100

    fix indentions

--- src/sliceBoard.cpp
+++ src/sliceBoard.cpp
@@ -46,277 +46,277 @@ Contact address: Computational Physics Group, Dept. of Physics,
 SliceBoard::SliceBoard(QWidget * parent)
     : QDialog(parent, Qt::WType_TopLevel)
 {
-	setWindowTitle( "AViz: Set Clipping" );
+    setWindowTitle( "AViz: Set Clipping" );
 
-	// Insert a grid that will hold spin boxes
-	// plus a row of control buttons
-	const int numCols = 5;
-        const int numRows = 5;
-        Q3GridLayout * sliceBox = new Q3GridLayout( this, numCols, numRows, SPACE, SPACE, "sliceBox" );
+    // Insert a grid that will hold spin boxes
+    // plus a row of control buttons
+    const int numCols = 5;
+    const int numRows = 5;
+    Q3GridLayout * sliceBox = new Q3GridLayout( this, numCols, numRows, SPACE, SPACE, "sliceBox" );
 
-	// Create a check box
-	sliceCb = new QCheckBox( this, "sliceCb" );
-	sliceCb->setText( "Slicing On" );
-	sliceBox->addWidget( sliceCb, 0, 0 );
+    // Create a check box
+    sliceCb = new QCheckBox( this, "sliceCb" );
+    sliceCb->setText( "Slicing On" );
+    sliceBox->addWidget( sliceCb, 0, 0 );
 
-	// Create a pushbutton to adjust to boundaries
-	QPushButton * boundaryPb = new QPushButton( this, "boundaryPb" );
-	boundaryPb->setText( "Set to Contour" ); 
-	sliceBox->addWidget( boundaryPb, 0, 4 );
+    // Create a pushbutton to adjust to boundaries
+    QPushButton * boundaryPb = new QPushButton( this, "boundaryPb" );
+    boundaryPb->setText( "Set to Contour" );
+    sliceBox->addWidget( boundaryPb, 0, 4 );
 
-	 // Define a callback for that button
-        QObject::connect( boundaryPb, SIGNAL(clicked()), this, SLOT(autoSlice()) );
+    // Define a callback for that button
+    QObject::connect( boundaryPb, SIGNAL(clicked()), this, SLOT(autoSlice()) );
 
-	// Create spin boxes, labels and check boxes
-        QLabel * sliceLX1 = new QLabel( this, "sliceXL1" );
-        sliceLX1->setText( " Slice X From: ");
-	sliceBox->addWidget( sliceLX1, 1, 0 );
+    // Create spin boxes, labels and check boxes
+    QLabel * sliceLX1 = new QLabel( this, "sliceXL1" );
+    sliceLX1->setText( " Slice X From: ");
+    sliceBox->addWidget( sliceLX1, 1, 0 );
 
     sliceSbX1 = new QFSpinBox( this );
-	sliceSbX1->setMinimum( MINSLI );
-	sliceSbX1->setMaximum( MAXSLI );
-	sliceBox->addWidget( sliceSbX1, 1, 1 );
+    sliceSbX1->setMinimum( MINSLI );
+    sliceSbX1->setMaximum( MAXSLI );
+    sliceBox->addWidget( sliceSbX1, 1, 1 );
 
-        QLabel * sliceLX2 = new QLabel( this, "sliceXL2" );
-        sliceLX2->setText( " To: ");
-	sliceBox->addWidget( sliceLX2, 1, 2 );
+    QLabel * sliceLX2 = new QLabel( this, "sliceXL2" );
+    sliceLX2->setText( " To: ");
+    sliceBox->addWidget( sliceLX2, 1, 2 );
 
     sliceSbX2 = new QFSpinBox( this );
-	sliceSbX2->setMinimum( MINSLI );
-	sliceSbX2->setMaximum( MAXSLI );
-	sliceBox->addWidget( sliceSbX2, 1, 3 );
+    sliceSbX2->setMinimum( MINSLI );
+    sliceSbX2->setMaximum( MAXSLI );
+    sliceBox->addWidget( sliceSbX2, 1, 3 );
 
-        QLabel * sliceLY1 = new QLabel( this, "sliceYL1" );
-        sliceLY1->setText( " Slice Y From: ");
-	sliceBox->addWidget( sliceLY1, 2, 0 );
+    QLabel * sliceLY1 = new QLabel( this, "sliceYL1" );
+    sliceLY1->setText( " Slice Y From: ");
+    sliceBox->addWidget( sliceLY1, 2, 0 );
 
     sliceSbY1 = new QFSpinBox( this );
-	sliceSbY1->setMinimum( MINSLI );
-	sliceSbY1->setMaximum( MAXSLI );
-	sliceBox->addWidget( sliceSbY1, 2, 1 );
+    sliceSbY1->setMinimum( MINSLI );
+    sliceSbY1->setMaximum( MAXSLI );
+    sliceBox->addWidget( sliceSbY1, 2, 1 );
 
-        QLabel * sliceLY2 = new QLabel( this, "sliceYL2" );
-        sliceLY2->setText( " To: ");
-	sliceBox->addWidget( sliceLY2, 2, 2 );
+    QLabel * sliceLY2 = new QLabel( this, "sliceYL2" );
+    sliceLY2->setText( " To: ");
+    sliceBox->addWidget( sliceLY2, 2, 2 );
 
     sliceSbY2 = new QFSpinBox( this );
-	sliceSbY2->setMinimum( MINSLI );
-	sliceSbY2->setMaximum( MAXSLI );
-	sliceBox->addWidget( sliceSbY2, 2, 3 );
+    sliceSbY2->setMinimum( MINSLI );
+    sliceSbY2->setMaximum( MAXSLI );
+    sliceBox->addWidget( sliceSbY2, 2, 3 );
 
-        QLabel * sliceLZ1 = new QLabel( this, "sliceZL1" );
-        sliceLZ1->setText( " Slice Z From: ");
-	sliceBox->addWidget( sliceLZ1, 3, 0 );
+    QLabel * sliceLZ1 = new QLabel( this, "sliceZL1" );
+    sliceLZ1->setText( " Slice Z From: ");
+    sliceBox->addWidget( sliceLZ1, 3, 0 );
 
     sliceSbZ1 = new QFSpinBox( this );
-	sliceSbZ1->setMinimum( MINSLI );
-	sliceSbZ1->setMaximum( MAXSLI );
-	sliceBox->addWidget( sliceSbZ1, 3, 1 );
+    sliceSbZ1->setMinimum( MINSLI );
+    sliceSbZ1->setMaximum( MAXSLI );
+    sliceBox->addWidget( sliceSbZ1, 3, 1 );
 
-        QLabel * sliceLZ2 = new QLabel( this, "sliceZL2" );
-        sliceLZ2->setText( " To: ");
-	sliceBox->addWidget( sliceLZ2, 3, 2 );
+    QLabel * sliceLZ2 = new QLabel( this, "sliceZL2" );
+    sliceLZ2->setText( " To: ");
+    sliceBox->addWidget( sliceLZ2, 3, 2 );
 
     sliceSbZ2 = new QFSpinBox( this );
-	sliceSbZ2->setMinimum( MINSLI );
-	sliceSbZ2->setMaximum( MAXSLI );
-	sliceBox->addWidget( sliceSbZ2, 3, 3 );
+    sliceSbZ2->setMinimum( MINSLI );
+    sliceSbZ2->setMaximum( MAXSLI );
+    sliceBox->addWidget( sliceSbZ2, 3, 3 );
 
-	 // Define a callback for the spin boxes
-        QObject::connect( sliceSbX1, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
-        QObject::connect( sliceSbX2, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
-        QObject::connect( sliceSbY1, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
-        QObject::connect( sliceSbY2, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
-        QObject::connect( sliceSbZ1, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
-        QObject::connect( sliceSbZ2, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
+    // Define a callback for the spin boxes
+    QObject::connect( sliceSbX1, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
+    QObject::connect( sliceSbX2, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
+    QObject::connect( sliceSbY1, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
+    QObject::connect( sliceSbY2, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
+    QObject::connect( sliceSbZ1, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
+    QObject::connect( sliceSbZ2, SIGNAL(valueChanged(int)), this, SLOT(sliceChanged()) );
 
-	// Create check boxes
-	indicateXCb = new QCheckBox( this, "indicateXSb" );
-	indicateXCb->setText( "Indicate Only" );
-	sliceBox->addWidget( indicateXCb, 1, 4 );
+    // Create check boxes
+    indicateXCb = new QCheckBox( this, "indicateXSb" );
+    indicateXCb->setText( "Indicate Only" );
+    sliceBox->addWidget( indicateXCb, 1, 4 );
 
-	indicateYCb = new QCheckBox( this, "indicateYSb" );
-	indicateYCb->setText( "Indicate Only" );
-	sliceBox->addWidget( indicateYCb, 2, 4 );
+    indicateYCb = new QCheckBox( this, "indicateYSb" );
+    indicateYCb->setText( "Indicate Only" );
+    sliceBox->addWidget( indicateYCb, 2, 4 );
 
-	indicateZCb = new QCheckBox( this, "indicateZSb" );
-	indicateZCb->setText( "Indicate Only" );
-	sliceBox->addWidget( indicateZCb, 3, 4 );
+    indicateZCb = new QCheckBox( this, "indicateZSb" );
+    indicateZCb->setText( "Indicate Only" );
+    sliceBox->addWidget( indicateZCb, 3, 4 );
 
-	 // Define a callback for these check boxes
-        QObject::connect( indicateXCb, SIGNAL(clicked()), this, SLOT(sliceChangedX()) );
-        QObject::connect( indicateYCb, SIGNAL(clicked()), this, SLOT(sliceChangedY()) );
-        QObject::connect( indicateZCb, SIGNAL(clicked()), this, SLOT(sliceChangedZ()) );
+    // Define a callback for these check boxes
+    QObject::connect( indicateXCb, SIGNAL(clicked()), this, SLOT(sliceChangedX()) );
+    QObject::connect( indicateYCb, SIGNAL(clicked()), this, SLOT(sliceChangedY()) );
+    QObject::connect( indicateZCb, SIGNAL(clicked()), this, SLOT(sliceChangedZ()) );
 
-	// Create a hboxlayout that will fill the lowest row
-	Q3HBox * hb = new Q3HBox( this, "hb" );
-	sliceBox->addMultiCellWidget( hb, numRows-1, numRows-1, 0, -1);
-	
-	// Create a placeholder 
-	QLabel * emptyL = new QLabel( hb, "emptyL" );
+    // Create a hboxlayout that will fill the lowest row
+    Q3HBox * hb = new Q3HBox( this, "hb" );
+    sliceBox->addMultiCellWidget( hb, numRows-1, numRows-1, 0, -1);
 
-	// Create pushbuttons that will go into the lowest row
-	QPushButton * done = new QPushButton( hb, "done" );
-	done->setText( "Done" ); 
+    // Create a placeholder
+    QLabel * emptyL = new QLabel( hb, "emptyL" );
 
-	 // Define a callback for that button
-        QObject::connect( done, SIGNAL(clicked()), this, SLOT(bdone()) );
+    // Create pushbuttons that will go into the lowest row
+    QPushButton * done = new QPushButton( hb, "done" );
+    done->setText( "Done" );
 
-	QPushButton * apply = new QPushButton( hb, "apply" );
-	apply->setText( "Apply" ); 
+    // Define a callback for that button
+    QObject::connect( done, SIGNAL(clicked()), this, SLOT(bdone()) );
 
-	 // Define a callback for that button
-        QObject::connect( apply, SIGNAL(clicked()), this, SLOT(bapply()) );
+    QPushButton * apply = new QPushButton( hb, "apply" );
+    apply->setText( "Apply" );
 
-	QPushButton * cancel = new QPushButton( hb, "cancel" );
-	cancel->setText( "Cancel" ); 
+    // Define a callback for that button
+    QObject::connect( apply, SIGNAL(clicked()), this, SLOT(bapply()) );
 
-	 // Define a callback for that button
-        QObject::connect( cancel, SIGNAL(clicked()), this, SLOT(bcancel()) );
+    QPushButton * cancel = new QPushButton( hb, "cancel" );
+    cancel->setText( "Cancel" );
 
-	hb->setStretchFactor( emptyL, 10 );
+    // Define a callback for that button
+    QObject::connect( cancel, SIGNAL(clicked()), this, SLOT(bcancel()) );
+
+    hb->setStretchFactor( emptyL, 10 );
 }
 
 
 // Set a pointer to the main form
 void SliceBoard::setMainFormAddress( MainForm * thisMF )
 {
-	mainForm = thisMF;
+    mainForm = thisMF;
 }
 
 
 // Get the current settings from the main form
 void SliceBoard::setSlice( viewParam vp )
 {
-	// Update the controls
-        sliceCb->setChecked( vp.slicing );
+    // Update the controls
+    sliceCb->setChecked( vp.slicing );
 
-        indicateXCb->setChecked( vp.showSlicePlaneX );
-        indicateYCb->setChecked( vp.showSlicePlaneY );
-        indicateZCb->setChecked( vp.showSlicePlaneZ );
+    indicateXCb->setChecked( vp.showSlicePlaneX );
+    indicateYCb->setChecked( vp.showSlicePlaneY );
+    indicateZCb->setChecked( vp.showSlicePlaneZ );
 
-	sliceSbX1->setValue( (int)floor(10.0*vp.sliceXMin+0.5) );
-	sliceSbX2->setValue( (int)floor(10.0*vp.sliceXMax+0.5) );
-	sliceSbY1->setValue( (int)floor(10.0*vp.sliceYMin+0.5) );
-	sliceSbY2->setValue( (int)floor(10.0*vp.sliceYMax+0.5) );
-	sliceSbZ1->setValue( (int)floor(10.0*vp.sliceZMin+0.5) );
-	sliceSbZ2->setValue( (int)floor(10.0*vp.sliceZMax+0.5) );
+    sliceSbX1->setValue( (int)floor(10.0*vp.sliceXMin+0.5) );
+    sliceSbX2->setValue( (int)floor(10.0*vp.sliceXMax+0.5) );
+    sliceSbY1->setValue( (int)floor(10.0*vp.sliceYMin+0.5) );
+    sliceSbY2->setValue( (int)floor(10.0*vp.sliceYMax+0.5) );
+    sliceSbZ1->setValue( (int)floor(10.0*vp.sliceZMin+0.5) );
+    sliceSbZ2->setValue( (int)floor(10.0*vp.sliceZMax+0.5) );
 }
 
 
 // Fill in slice values corresponding to viewing the entire set
 void SliceBoard::autoSlice()
 {
-        // Get the current settings
-        if (mainForm) {
-                viewParam thisVp = (*mainForm->getViewParam());
-                viewObject thisVo = (*mainForm->getViewObject());
+    // Get the current settings
+    if (mainForm) {
+        viewParam thisVp = (*mainForm->getViewParam());
+        viewObject thisVo = (*mainForm->getViewObject());
 
-		thisVp.sliceXMin = thisVo.xmin-SLICEEPS;
-		thisVp.sliceXMax = thisVo.xmax+SLICEEPS;
-		thisVp.sliceYMin = thisVo.ymin-SLICEEPS;
-		thisVp.sliceYMax = thisVo.ymax+SLICEEPS;
-		thisVp.sliceZMin = thisVo.zmin-SLICEEPS;
-		thisVp.sliceZMax = thisVo.zmax+SLICEEPS;
+        thisVp.sliceXMin = thisVo.xmin-SLICEEPS;
+        thisVp.sliceXMax = thisVo.xmax+SLICEEPS;
+        thisVp.sliceYMin = thisVo.ymin-SLICEEPS;
+        thisVp.sliceYMax = thisVo.ymax+SLICEEPS;
+        thisVp.sliceZMin = thisVo.zmin-SLICEEPS;
+        thisVp.sliceZMax = thisVo.zmax+SLICEEPS;
 
-		this->setSlice( thisVp );
-	}
+        this->setSlice( thisVp );
+    }
 }
 
 
 // Respond to changes of the boundary values
 void SliceBoard::sliceChanged()
 {
-	// Always activate the slicing-on toggle when values change
-        sliceCb->setChecked( TRUE );
+    // Always activate the slicing-on toggle when values change
+    sliceCb->setChecked( TRUE );
 }
 
 
 // Respond to changes of the boundary values
 void SliceBoard::sliceChangedX()
 {
-	// Always activate the slicing-on toggle when values change
-        sliceCb->setChecked( TRUE );
+    // Always activate the slicing-on toggle when values change
+    sliceCb->setChecked( TRUE );
 }
 
 
 // Respond to changes of the boundary values
 void SliceBoard::sliceChangedY()
 {
-	// Always activate the slicing-on toggle when values change
-        sliceCb->setChecked( TRUE );
+    // Always activate the slicing-on toggle when values change
+    sliceCb->setChecked( TRUE );
 }
 
 
 // Respond to changes of the boundary values
 void SliceBoard::sliceChangedZ()
 {
-	// Always activate the slicing-on toggle when values change
-        sliceCb->setChecked( TRUE );
+    // Always activate the slicing-on toggle when values change
+    sliceCb->setChecked( TRUE );
 }
 
 
 // Read the current clip settings
 void SliceBoard::registerSettings( void )
 {
-        // Get the current settings
-        if (mainForm) {
-                viewParam * vp = mainForm->getViewParam( );
+    // Get the current settings
+    if (mainForm) {
+        viewParam * vp = mainForm->getViewParam( );
 
-		(*vp).slicing = sliceCb->isChecked();
+        (*vp).slicing = sliceCb->isChecked();
 
-		(*vp).showSlicePlaneX = indicateXCb->isChecked();
-		(*vp).showSlicePlaneY = indicateYCb->isChecked();
-		(*vp).showSlicePlaneZ = indicateZCb->isChecked();
+        (*vp).showSlicePlaneX = indicateXCb->isChecked();
+        (*vp).showSlicePlaneY = indicateYCb->isChecked();
+        (*vp).showSlicePlaneZ = indicateZCb->isChecked();
 
-	        (*vp).sliceXMin = (float)sliceSbX1->value()/10.0;
-	        (*vp).sliceXMax = (float)sliceSbX2->value()/10.0;
-	        (*vp).sliceYMin = (float)sliceSbY1->value()/10.0;
-	        (*vp).sliceYMax = (float)sliceSbY2->value()/10.0;
-	        (*vp).sliceZMin = (float)sliceSbZ1->value()/10.0;
-	        (*vp).sliceZMax = (float)sliceSbZ2->value()/10.0;
+        (*vp).sliceXMin = (float)sliceSbX1->value()/10.0;
+        (*vp).sliceXMax = (float)sliceSbX2->value()/10.0;
+        (*vp).sliceYMin = (float)sliceSbY1->value()/10.0;
+        (*vp).sliceYMax = (float)sliceSbY2->value()/10.0;
+        (*vp).sliceZMin = (float)sliceSbZ1->value()/10.0;
+        (*vp).sliceZMax = (float)sliceSbZ2->value()/10.0;
 
-		(*vp).slicingSet = TRUE;
-	}
+        (*vp).slicingSet = TRUE;
+    }
 }
 
 
 // Accept the setting: update the rendering and hide the board
 void SliceBoard::bdone()
 {
-        this->registerSettings();
+    this->registerSettings();
 
-        // Re-do the graphics, using the new view settings
-        if (mainForm) {
+    // Re-do the graphics, using the new view settings
+    if (mainForm) {
 
-		// Re-define the view object and re-draw
-                mainForm->updateView();
-                mainForm->updateRendering();
-	}
+        // Re-define the view object and re-draw
+        mainForm->updateView();
+        mainForm->updateRendering();
+    }
 
-	// Hide now
-        hide();
+    // Hide now
+    hide();
 }
 
 
 // Accept the setting: update the rendering 
 void SliceBoard::bapply()
 {
-        this->registerSettings();
+    this->registerSettings();
 
-        // Re-do the graphics, using the new view settings
-        if (mainForm) {
+    // Re-do the graphics, using the new view settings
+    if (mainForm) {
 
-		// Re-define the view object and re-draw
-                mainForm->updateView();
-                mainForm->updateRendering();
-	}
+        // Re-define the view object and re-draw
+        mainForm->updateView();
+        mainForm->updateRendering();
+    }
 }
 
 
 // Cancel the setting: hide the board
 void SliceBoard::bcancel()
 {
-	// Hide now
-	hide();
+    // Hide now
+    hide();
 }
